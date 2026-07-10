8495d9d455506a1585c9539a13c05f87b9990326
brunoBarros45 <brunobarros770@gmail.com>
2026-07-10 15:12:27 -0300

    arregle el duplicado const

--- src/components/FormContratista.js
+++ src/components/FormContratista.js
@@ -1,24 +1,13 @@
 import React, { useState } from "react"
 import { Pressable, Text, TextInput, View } from "react-native"
 import { saveContratista } from "../database/Contratistas"
-import { useDispatch, useSelector } from 'react-redux';
-import { addContratista } from "../redux/ContratistaFormSlice";
-
-
-
-export const FormContratista = () => {
-
-  const [nombre, setNombre] = useState()
-  const [especialidad, setEspecialidad] = useState()
-  const [telefono, setTelefono] = useState()
-  const [apellido, setapellido] = useState()
-
-  const dispatch = useDispatch();
-
-import styles from "../styles" // Tu archivo de estilos externo
+import { useDispatch, useSelector } from "react-redux"
+import { addContratista } from "../redux/ContratistaFormSlice"
+import styles from "../styles"
 import { Contratistas } from "./Contratistas"
 
 export const FormContratista = () => {
+  const dispatch = useDispatch()
   const [nombre, setNombre] = useState("")
   const [apellido, setapellido] = useState("")
   const [especialidad, setEspecialidad] = useState("")
@@ -33,34 +22,16 @@ export const FormContratista = () => {
   return (
     <View style={styles.container}>
       <View style={styles.formGroup}>
-        <TextInput 
-          style={styles.input} 
-          placeholder="Nombre" 
+        <TextInput style={styles.input} placeholder="Nombre" placeholderTextColor="#1B365D" value={nombre} onChangeText={setNombre} />
+        <TextInput style={styles.input} placeholder="Apellido" placeholderTextColor="#1B365D" value={apellido} onChangeText={setapellido} />
+        <TextInput
+          style={styles.input}
+          placeholder="Especialidad"
           placeholderTextColor="#1B365D"
-          value={nombre} 
-          onChangeText={setNombre} 
+          value={especialidad}
+          onChangeText={setEspecialidad}
         />
-        <TextInput 
-          style={styles.input} 
-          placeholder="Apellido" 
-          placeholderTextColor="#1B365D"
-          value={apellido} 
-          onChangeText={setapellido} 
-        />
-        <TextInput 
-          style={styles.input} 
-          placeholder="Especialidad" 
-          placeholderTextColor="#1B365D"
-          value={especialidad} 
-          onChangeText={setEspecialidad} 
-        />
-        <TextInput 
-          style={styles.input} 
-          placeholder="Teléfono" 
-          placeholderTextColor="#1B365D"
-          value={telefono} 
-          onChangeText={setTelefono} 
-        />
+        <TextInput style={styles.input} placeholder="Teléfono" placeholderTextColor="#1B365D" value={telefono} onChangeText={setTelefono} />
       </View>
 
       <View>
@@ -69,9 +40,9 @@ export const FormContratista = () => {
         </Pressable>
       </View>
 
-      {/* Lista de contratistas abajo */}
       <View style={{ marginTop: 20 }}>
         <Contratistas />
       </View>
     </View>
-  )}
+  )
+}
